0258eaefbbee50fde36bc147381c7171fdb3a249
timfors <tkhabibullin2014@litsey2.ru>
2019-02-22 19:13:17 +0300

    +

---
+++ Assets/Scripts/PlanetData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Planet" , menuName = "SciptableObjects/Planets")]
+public class Planets : ScriptableObject
+{
+    [SerializeField]
+    private int speed;
+
+    [SerializeField]
+    private Sprite icon;
+
+
+    public int Speed
+    {
+        get
+        {
+            return speed;
+        }
+    }
+
+    public Sprite Icon
+    {
+        get
+        {
+            return icon;
+        }
+    }
+}
