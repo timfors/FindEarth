5cc2d39474709d7d78b3c018b03ea99ac97c6cd6
timfors <tkhabibullin2014@litsey2.ru>
2019-03-06 20:02:10 +0300

    field swap

---
+++ Assets/Scripts/FieldMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldMove : MonoBehaviour
+{
+    public Transform cam;
+
+    private Vector3 mov = new Vector3(36, 0, 0);  
+
+    void Update()
+    {
+        if (cam.position.x - transform.position.x >= 18)
+        {
+            transform.position += mov; 
+        }
+        else if (cam.position.x - transform.position.x <= -18)
+        {
+            transform.position -= mov;
+        }
+    }
+}
